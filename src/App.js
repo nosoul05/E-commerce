0028768fad4e88ec2fcef1a4994b2afcf549e2f2
Chris <nousoul05@gmail.com>
2021-10-03 17:20:13 -0400

    Adding a few more files.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import  { Switch, Route } from 'react-router-dom';
 import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component';
 // import HomePage from '../../src/homepage.component';
 
+const HatsPage = () => (
+
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TestPage = () => (
+<div>
+  <h1>Test Page</h1>
+</div>
+
+);
+
 function App() {
   return (
-    <div>
-  <HomePage/>
-    </div>
+  <div>
+    <Switch>
+      <Route exact path='/E-commerce' component={HomePage} />
+      <Route path='/E-commerce/hats' component={HatsPage} />
+      <Route path='/testpage' component={TestPage}></Route>
+    </Switch>
+  </div>
   );
 }
 
