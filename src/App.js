27ef7a2efd3ffca5ed92f6144f562c579060a682
Chris Robinson <nosoul05@gmail.com>
2021-10-03 23:31:55 -0400

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import  { Switch, Route, Link } from 'react-router-dom';
+import  { Switch, Route } from 'react-router-dom'; //Removed Link
 import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component';
 // import HomePage from '../../src/homepage.component';
+import ShopPage from './pages/shop/shop.compontent';
 
-const HatsPage = () => (
 
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () => (
 
-const TopicsList = (props) => {
-  console.log(props);
-  return(
-      <div>
-        <h1>TOPIC LIST PAGE</h1>
-      </div>
-    );
-};
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-  <div>
-    <h1>Topic Detail Page: {props.match.params.topicId} </h1>
-  </div>
+// const TopicsList = (props) => {
+//   console.log(props);
+//   return(
+//       <div>
+//         <h1>TOPIC LIST PAGE</h1>
+//       </div>
+//     );
+// };
 
-  );
-};
+// const TopicDetail = (props) => {
+//   console.log(props);
+//   return (
+//   <div>
+//     <h1>Topic Detail Page: {props.match.params.topicId} </h1>
+//   </div>
+
+//   );
+// };
 
 function App() {
   return (
   <div>
     <Switch>
       <Route exact path='/E-commerce' component={HomePage} />
-      <Route path='/E-commerce/hats' component={HatsPage} />
+      <Route path='/E-commerce/shop' component={ShopPage} />
+      {/* <Route path='/E-commerce/hats' component={HatsPage} />
       <Route exact path='/E-commerce/topics' component={TopicsList}></Route>
-      <Route path='/E-commerce/topics/:topicId' component={TopicDetail} />
+      <Route path='/E-commerce/topics/:topicId' component={TopicDetail} /> */}
     </Switch>
   </div>
   );
