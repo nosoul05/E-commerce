a8e0580822a3bf59de54144864a577ea4128be2b
Chris <nousoul05@gmail.com>
2021-10-09 12:08:15 -0400

    added redux to project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
+
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
@@ -12,10 +17,13 @@ ReactDOM.render(
   //   <App />
   // </React.StrictMode>,
   // document.getElementById('root')
+
+  <Provider store={store}>
   <BrowserRouter>
   
   <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 
 
---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,6 @@
+export const setCurrentUser = user => ({
+
+    type:'SET_CURRENT_USER', //ensure  this is the same as in the user reducer
+    payload: user
+
+});
