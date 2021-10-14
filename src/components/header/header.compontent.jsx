8c42be141539f45d6940ce889276f765cabe969b
Chris <nousoul05@gmail.com>
2021-10-13 20:33:13 -0400

    Added cart dropdown toggle

--- src/components/header/header.compontent.jsx
+++ src/components/header/header.compontent.jsx
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
           <Logo className='logo'/>
@@ -19,22 +20,45 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='E-commerce/shop'>
                 Contact
             </Link>
-            {
+            {currentUser ?
+            (
+            
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                    </div>
+            
+        
+            ) :
+            (
+                <Link className='option' to='/signin'>
+                    SIGN IN
+                </Link>
+            )}
+            <CartIcon />
+
+            {/* {
                 currentUser ?
                 <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                 :
                 <Link className='option' to='/E-commerce/signin'>SIGN IN</Link>
 
-            }
+            } */}
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
+       =
     </div>
 
 );
 
-const mapStateToProps = state => ({
+const mapStateToProps = ({user: {currentUser}, cart: {hidden} }) => ({
 
-    currentUser: state.user.currentUser
+    // currentUser: state.user.currentUser,
+    currentUser,
+    hidden
 
 });
 
-export default connect(mapStateToProps) (Header);
+export default connect(mapStateToProps)(Header);
